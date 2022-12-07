b595b19faaeee5b3ee0a2511f83387a9f3b596a8
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-07 22:23:54 +0100

    Update Architecture and Add Walker Test Cases

---
+++ TestWalkingInTheWild/TestsWalker.cs
@@ -0,0 +1,167 @@
+using WalkingInTheWild;
+using static WalkingInTheWild.Cloth;
+using static WalkingInTheWild.Walker;
+
+namespace TestWalkingInTheWild
+{
+    public class TestsWalker
+    {
+        //region private attributes
+        private Walker walker;
+        private string pseudo;
+        //endregion private attributes
+
+        [SetUp]
+        public void Setup()
+        {
+            pseudo = "Pseudo";
+            walker = new Walker(pseudo);    
+        }
+
+        [Test]
+        public void Properties_AfterInstantiationDefaultValues_PropertiesAreCorrecltyReturned()
+        {
+            //given
+            //refer to Setup()
+
+            //when
+            //constructor is called in Setup() 
+
+            //then
+            Assert.AreEqual(pseudo, walker.Pseudo);
+            Assert.IsNull(walker.Bagpack);
+        }
+
+        [Test]
+        public void TakeBagpack_WalkerReady_BagpackTaken()
+        {
+            //given
+            //refer to Setup()
+            Bagpack bagpack = new Bagpack(20.00f);
+            Assert.Null(walker.Bagpack);
+
+            //when
+            this.walker.TakeBagpack(bagpack);
+
+            //then
+            Assert.AreEqual(bagpack, walker.Bagpack);
+        }
+
+        [Test]
+        public void TakeBagpack_WalkerNotReady_ThrowException()
+        {
+            //given
+            //refer to Setup()
+            Bagpack bagpack = new Bagpack(20.00f);
+            walker.TakeBagpack(bagpack);
+            this.walker.TakeBagpack(bagpack);
+            Assert.NotNull(walker.Bagpack);
+
+            //when
+            //Event is called by the assertion
+
+            //then
+            Assert.Throws<WalkerNotReadyException>(() => this.walker.TakeBagpack(bagpack));
+        }
+        
+        [Test]        
+        public void DropBagpack_WalkerIsCarringABagpack_WalkerDropsTheBagpack()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void DropBagpack_WalkerIsNotCarringABagpack_ThrowException()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_BagpackAvailableLoadSingleCloth_ClothIsLoadedInBagpack()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_BagpackAvailableLoadMultipleCloths_ClothsAreLoadedInBagpack()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_ClothBagpackNotAvailable_ThrowException()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_BagpackAvailableLoadSingleEquipment_EquipmentIsLoadedInBagpack()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_BagpackAvailableLoadMultipleEquipments_EquipmentAreLoadedInBagpack()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void LoadBagpack_EquipmentBagpackNotAvailable_ThrowException()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void EmptyBagpack_BagpackContainsClothsAndEquipment_BackpackIsEmpty()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+
+        [Test]
+        public void EmptyBagpack_BagpackDoesntContainNeitherClothsOrEquipment_ThrowException()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+    }
+}
--- WalkingInTheWild/Walker.cs
+++ WalkingInTheWild/Walker.cs
@@ -2,19 +2,68 @@
 {
     public class Walker
     {
-        //region private attributes
+        #region private attributes
         private string _pseudo;
         private Bagpack? _backpack;
-        //endregion private attributes
+        #endregion private attributes
 
-        //region public methods
+        #region public methods
         public Walker(string pseudo)
         {
-            throw new NotImplementedException();  
+            throw new NotImplementedException();
         }
-        //endregion public methods
 
-        //region private methods
-        //endregion private methods
+        public string Pseudo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Bagpack? Bagpack
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void TakeBagpack(Bagpack bagpack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DropBagpack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadBagpack(List<Cloth> cloths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadBagpack(List<Equipment> equipments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EmptyBagpack()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion public methods
+
+        #region private methods
+        #endregion private methods
+
+        #region nested classes
+        public class WalkerException:Exception{}
+        public class WalkerNotReadyException : WalkerException { }
+        public class EmptyBagpackException : WalkerException { }
+        #endregion nested classes
+
+
     }
 }
