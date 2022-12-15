57d09eefb0c1b7234b507041d0a61ef2b1906cca
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-12-15 11:47:23 +0100

    fix:update bagpack maxload to avoid inententional exception case

--- TestWalkingInTheWild/TestsWalker.cs
+++ TestWalkingInTheWild/TestsWalker.cs
@@ -10,7 +10,7 @@ namespace TestWalkingInTheWild
         private Walker _walker;
         private string _pseudo;
         private Bagpack _bagpack = null;
-        private float _maxLoad = 25.50f;
+        private float _maxLoad = 60.50f;
         //endregion private attributes
 
         [SetUp]
@@ -168,7 +168,7 @@ namespace TestWalkingInTheWild
             _walker.LoadBagpack(Utils.GenerateEquipment(10));
 
             //then
-            Assert.That(_walker.Bagpack.Equipments.Count, Is.EqualTo(1));
+            Assert.That(_walker.Bagpack.Equipments.Count, Is.EqualTo(10));
             Assert.That(_walker.Bagpack.RemainingLoadCapacity, Is.EqualTo(_maxLoad - 55));
         }
 
